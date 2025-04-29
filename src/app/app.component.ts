a8027f4e7e98f2d5364c27819fbb3efc793df968
Kyrubi <u20221c486@upc.edu.pe>
2025-04-29 03:39:39 -0500

    fix: in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,29 +5,30 @@ import {ToolbarComponent} from './public/components/toolbar/toolbar.component';
 import {University} from './universities/model/university.entity';
 import {HipoLabsApiService} from './universities/services/hipo-labs-api.service';
 import {LogoApiService} from './shared/services/logo-api.service';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
+    UniversityListComponent,
     FooterComponent,
-    ToolbarComponent,
-    UniversityListComponent
+    ToolbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  universities: University[] = [];
-
+export class AppComponent implements OnInit{
+  universities: University[] = []
   title = 'university-finder';
+
   private hipoLabsApi = inject(HipoLabsApiService);
   private logoApi = inject(LogoApiService);
 
-  ngOnInit() {
+  ngOnInit(){
     this.hipoLabsApi.getUniversities().subscribe(universities => {
-      this.universities = universities
-      this.universities.forEach(university => university.domainLogo = this.logoApi.getUrlToLogo(university))
-    })
+      this.universities = universities;
+      this.universities.forEach(university => university.domainLogo = this.logoApi.getUrlToLogo(university));
+      console.log(this.universities);
+    });
   }
 }
-
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  universitiesProviderApiBaseUrl: 'http://universities.hipolabs.com',  // URL base del show
-  universitiesProviderSourcesEndpointPath: '/search?name=ciencias',
+  production: false,
+  hipoLabsProviderApiBaseUrl: 'http://universities.hipolabs.com',  // URL base del show
+  hipoLabsProviderUniversitiesEndpointPath: '/search?name=ciencias',
   logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
 };
